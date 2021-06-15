5d5645f5ebd9da4edcd972cd3f1d8c36a09a63f7
Philip McNamara <philipmcnamara1@gmail.com>
2021-06-15 21:23:35 +0100

    Show movie review excerpts; Refactor Details page

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -1,32 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router-dom";
-import MovieDetails from "../components/movieDetails";
-import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import MovieHeader from "../headerMovie";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import { getMovieImages } from "../../api/tmdb-api";
 
-const MovieDetailsPage = (props) => {
-  const { id } = props.match.params;
-  const [movie, setMovie] = useState(null);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  gridList: {
+    width: 450,
+    height: '100vh',
+  },
+}));
+
+const TemplateMoviePage = ({ movie, children }) => {
+  const classes = useStyles();
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
+    getMovieImages(movie.id).then((images) => {
+      setImages(images);
     });
-  }, [id]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      {movie ? (
-        <>
-          <PageTemplate movie={movie}>
-            <MovieDetails movie={movie} />
-          </PageTemplate>
-        </>
-      ) : (
-        <p>Waiting for movie details</p>
-      )}
+      <MovieHeader movie={movie} />
+
+      <Grid container spacing={5} style={{ padding: "15px" }}>
+        <Grid item xs={3}>
+          <div className={classes.root}>
+            <GridList cellHeight={500} className={classes.gridList} cols={1}>
+              {images.map((image) => (
+                <GridListTile key={image.file_path} className={classes.gridListTile} cols={1}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                    alt={image.poster_path}
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
+        </Grid>
+
+        <Grid item xs={9}>
+          {children}
+        </Grid>
+      </Grid>
     </>
   );
 };
 
-export default withRouter(MovieDetailsPage);
+export default TemplateMoviePage;
