4f53f87f35a491c0980fa373bb11b0a21254fd2b
Philip McNamara <philipmcnamara1@gmail.com>
2021-06-16 12:48:53 +0100

    Full review page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
-import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
   return (
