468d335a78bce4ce85b1348a5888e757794be025
Philip McNamara <philipmcnamara1@gmail.com>
2021-06-29 19:02:51 +0100

    Tagging upcoming movies as must-watch.

--- src/components/cardIcons/addToPlaylist.js
+++ src/components/cardIcons/addToPlaylist.js
@@ -5,6 +5,7 @@ import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
 
 const AddToPlaylistIcon = ({ movie }) => {
   const context = useContext(MoviesContext);
+  
   const handleAddToPlaylist = (e) => {
     e.preventDefault();
     context.addToPlaylist(movie);
--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -8,6 +8,7 @@ import CardHeader from "@material-ui/core/CardHeader";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
 import CalendarIcon from "@material-ui/icons/CalendarTodayTwoTone";
 import StarRateIcon from "@material-ui/icons/StarRate";
 import IconButton from "@material-ui/core/IconButton";
@@ -28,6 +29,7 @@ const useStyles = makeStyles({
 export default function MovieCard({ movie, action }) {
   const classes = useStyles();
   const { favorites, addToFavorites } = useContext(MoviesContext);
+  const { playlist, addToPlaylist } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
     movie.favorite = true;
@@ -40,6 +42,11 @@ export default function MovieCard({ movie, action }) {
     addToFavorites(movie);
   };
 
+  const handleAddToPlaylist = (e) => {
+    e.preventDefault();
+    addToPlaylist(movie);
+  };
+
   return (
     <Card className={classes.card}>
       <CardHeader
@@ -48,6 +55,7 @@ export default function MovieCard({ movie, action }) {
         movie.favorite ? (
           <Avatar className={classes.avatar}>
             <FavoriteIcon />
+
           </Avatar>
         ) : null
       }
